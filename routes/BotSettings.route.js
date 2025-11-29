b3b04ffe788f424a9a6d2ced91519f4ad20a338e
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-29 16:00:02 +0530

    modified

--- routes/BotSettings.route.js
+++ routes/BotSettings.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-
 const botSettingsRouter = express.Router();
+
+import {
+	getBotSettings,
+	updateBotSettings,
+} from "../controllers/botsettings.controller.js";
+
+botSettingsRouter.get("/settings", getBotSettings);
+botSettingsRouter.post("/settings", updateBotSettings);
 
 export default botSettingsRouter;
